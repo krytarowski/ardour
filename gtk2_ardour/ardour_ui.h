4c01a38f838558a2ae2e2079e210eba72a90fcbc
Paul Davis <paul@linuxaudiosystems.com>
2015-07-27 18:05:42 -0400

    remove all tearoffs except the monitor section.
    We don't need this functionality anymore as we build on 15 years experience plus the new tabbed structure

--- gtk2_ardour/ardour_ui.h
+++ gtk2_ardour/ardour_ui.h
@@ -141,10 +141,6 @@ namespace ARDOUR {
 	class ProcessThread;
 }
 
-namespace Gtkmm2ext {
-	class TearOff;
-}
-
 namespace Gtk {
 	class ProgressBar;
 }
@@ -223,7 +219,7 @@ class ARDOUR_UI : public Gtkmm2ext::UI, public ARDOUR::SessionHandlePtr
 	XMLNode* mixer_settings () const;
 	XMLNode* keyboard_settings () const;
 	XMLNode* tearoff_settings (const char*) const;
-
+	
 	void save_ardour_state ();
 	gboolean configure_handler (GdkEventConfigure* conf);
 
@@ -302,8 +298,6 @@ class ARDOUR_UI : public Gtkmm2ext::UI, public ARDOUR::SessionHandlePtr
 	void show_ui_prefs ();
 
 	bool check_audioengine(Gtk::Window&);
-
-	void update_tearoff_visibility ();
 
 	void setup_profile ();
 	void setup_tooltips ();
@@ -425,15 +419,9 @@ class ARDOUR_UI : public Gtkmm2ext::UI, public ARDOUR::SessionHandlePtr
 
 	std::list<MidiTracer*> _midi_tracer_windows;
 
-	void detach_tearoff (Gtk::Box* parent, Gtk::Widget* contents);
-	void reattach_tearoff (Gtk::Box* parent, Gtk::Widget* contents, int32_t order);
-	void reattach_all_tearoffs ();
-
 	/* Transport Control */
 
-	Gtkmm2ext::TearOff*      transport_tearoff;
 	Gtk::Frame               transport_frame;
-	Gtk::HBox                transport_tearoff_hbox;
 	Gtk::HBox                transport_hbox;
 	Gtk::Fixed               transport_base;
 	Gtk::Fixed               transport_button_base;
